a3379a56ffbd0233390c04fbddb3090c4f3a7d74
Sören Gade <gade.soeren@googlemail.com>
2013-11-19 00:44:26 +0100

    Moved test to /spec.

---
+++ test/spec/scrolling.js
@@ -0,0 +1,27 @@
+describe('jquery-smoothscroll', function() {
+  describe('#smoothScrollTop', function() {
+    
+    it('should return this at the end', function() {
+      
+      var self = $("#page").smoothScrollTop();
+      assert(self !== undefined);
+      
+    });
+    
+    it('should not crash on empty selectors', function() {
+      
+      var self = $("").smoothScrollTop();
+      assert(self !== undefined);
+      
+    });
+    
+    it('should scroll to first element but return collection', function() {
+      
+      var $elements = $("h1");
+      var self = $elements.smoothScrollTop();
+      assert(self == $elements);
+      
+    });
+    
+  });
+});
