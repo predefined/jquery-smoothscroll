47c737c21283121941763bf642093f213df83b5f
Sören Gade <gade.soeren@googlemail.com>
2013-11-18 23:51:32 +0100

    Made test run in browser.

---
+++ test/specs/scrolling.js
@@ -0,0 +1,13 @@
+describe('jquery-smoothscroll', function() {
+  describe('#smoothScrollTop', function() {
+    
+    it('should return this at the end', function() {
+      
+      var self = $("#page").smoothScrollTop();
+      assert(self !== undefined);
+      
+      
+    });
+    
+  });
+});
